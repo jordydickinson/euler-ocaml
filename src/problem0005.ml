ddd08466a024e9f7a52eddacb5e8c5369bfb9b06
Jordy Dickinson <jordy.dickinson@icloud.com>
2019-12-02 10:57:36 -0500

    Improve performance of problem 5

--- src/problem0005.ml
+++ src/problem0005.ml
@@ -8,14 +8,36 @@
 
 open Core_kernel
 
-let rec divisible_thru i j n =
-  if i > j then true
-  else if n % i = 0 then divisible_thru (i + 1) j n
-  else false
+let multiplicity n d =
+  let rec multiplicity' accum n =
+    if n % d = 0
+    then multiplicity' (accum + 1) (n/d)
+    else accum
+  in
+  multiplicity' 0 n
+
+let factors n =
+  let rec factors' accum primes n =
+    if n = 1 then accum else
+    let p = Sequence.hd_exn primes in
+    if p > n then accum else
+    let primes = Sequence.tl_eagerly_exn primes in
+    let m = multiplicity n p in
+    if m = 0 then factors' accum primes n else
+    let n = n/(m*p) in
+    let accum = Map.add_exn accum p m in
+    factors' accum primes n
+  in
+  factors' (Map.empty (module Int)) Numbers.primes n
+
+let factors_union ps1 ps2 =
+  Map.merge_skewed ps1 ps2 ~combine:(fun ~key p1 p2 -> max p1 p2)
 
 let solve () =
-  let rec loop n =
-    if divisible_thru 1 20 n then n
-    else loop (n + 1) in
-  loop 1 |> string_of_int
+  Sequence.range 2 21
+  |> Sequence.map ~f:factors
+  |> Sequence.fold ~init:(Map.empty (module Int)) ~f:factors_union
+  |> Map.fold ~init:1
+      ~f:(fun ~key ~data product -> product * Int.(key ** data))
+  |> string_of_int
   (* 232792560 *)
