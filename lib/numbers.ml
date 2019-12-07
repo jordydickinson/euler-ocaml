922dc1313a13663f0b7d7589756593045769a3e4
Jordy Dickinson <jordy.dickinson@icloud.com>
2019-12-06 19:47:35 -0500

    Optimize Numbers.permutations (slightly)

--- lib/numbers.ml
+++ lib/numbers.ml
@@ -155,13 +155,20 @@ let perm_of_rank len rank =
   let fac = factoradic_of_int rank in
   perm_of_factoradic len fac
 
-let rec permutations xs =
-  let seq = Sequence.of_lazy @@ lazy begin
-    match xs with
-    | [] -> Sequence.singleton []
-    | x :: xs ->
-      let perms1 = Sequence.map (permutations xs) ~f:(List.cons x) in
-      let perms2 = permutations (xs @ [x]) in
-      Sequence.append perms1 perms2
-  end in
-  Sequence.take seq (factorial @@ List.length xs)
+let permutations xs =
+  let rec permutations' xs xslen permslen =
+    let seq = Sequence.of_lazy @@ lazy begin
+      match xs with
+      | [] -> Sequence.singleton []
+      | x :: xs ->
+        let perms1 =
+          Sequence.map ~f:(List.cons x)
+            (permutations' xs (xslen - 1) (permslen/xslen))
+        in
+        let perms2 = permutations' (xs @ [x]) xslen permslen in
+        Sequence.append perms1 perms2
+    end in
+    Sequence.take seq permslen
+  in
+  let xslen = List.length xs in
+  permutations' xs xslen (factorial xslen)
